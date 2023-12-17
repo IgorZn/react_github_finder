3de470175da35b24ae74956101e72eeeb6364af8
igor.znamensky <igor.znamensky@gmail.com>
2023-12-17 11:59:09 +0300

    8 Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import {BrowserRouter, Router, Route} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
     return (
-        <button className="btn btn-primary">Button</button>
+        <BrowserRouter>
+            <div className="flex flex-col justify-between h-screen">
+                <Navbar/>
+                <main>
+                    Content
+                </main>
+            </div>
+        </BrowserRouter>
     );
 }
 

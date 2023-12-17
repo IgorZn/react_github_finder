8bcbc115a8e9aa8a7f634522243948b677037116
igor.znamensky <igor.znamensky@gmail.com>
2023-12-17 11:41:05 +0300

    8 Navbar Component

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  plugins: [require("daisyui")],
+}

87e37cde6e489aa2334d07b38cceeecccc0751ac
igor.znamensky <igor.znamensky@gmail.com>
2023-12-18 10:25:57 +0300

    8 Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {BrowserRouter, Router, Route} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="flex flex-col justify-between h-screen">
                 <Navbar/>
-                <main>
+                <main className={'container mx-auto px-3 pb-12'}>
                     Content
                 </main>
+                <Footer/>
             </div>
         </BrowserRouter>
     );

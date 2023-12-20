78b082f940ed6e75c8a675f255c2e970c830a3ae
igor.znamensky <igor.znamensky@gmail.com>
2023-12-20 10:01:14 +0300

    9 Github API & Getting Token

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import {BrowserRouter, Router, Route} from "react-router-dom";
+import {BrowserRouter, Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
@@ -8,8 +11,15 @@ function App() {
             <div className="flex flex-col justify-between h-screen">
                 <Navbar/>
                 <main className={'container mx-auto px-3 pb-12'}>
+                    <Routes>
+                        <Route path={'/about'} element={<About/>}/>
+                        <Route path={'/'} element={<Home/>}/>
+                        <Route path={'/notfound'} element={<NotFound/>}/>
+                        <Route path={'/*'} element={<NotFound/>}/>
+                    </Routes>
                     Content
                 </main>
+
                 <Footer/>
             </div>
         </BrowserRouter>

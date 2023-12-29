14996d0c31bfc7033fa5ca1217f2110230835475
igor.znamensky <igor.znamensky@gmail.com>
2023-12-29 09:35:00 +0300

    10 Alert Component

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import {GithubProvider} from "./context/github/GithubContext";
+import {AlertProvider} from './context/alert/AlertContext'
 
 function App() {
     return (
         <GithubProvider>
-            <BrowserRouter>
-                <div className="flex flex-col justify-between h-screen">
-                    <Navbar/>
-                    <main className={'container mx-auto px-3 pb-12'}>
-                        <Routes>
-                            <Route path={'/about'} element={<About/>}/>
-                            <Route path={'/'} element={<Home/>}/>
-                            <Route path={'/notfound'} element={<NotFound/>}/>
-                            <Route path={'/*'} element={<NotFound/>}/>
-                        </Routes>
-                    </main>
+            <AlertProvider>
+                <BrowserRouter>
+                    <div className="flex flex-col justify-between h-screen">
+                        <Navbar/>
+                        <main className={'container mx-auto px-3 pb-12'}>
+                            <Routes>
+                                <Route path={'/about'} element={<About/>}/>
+                                <Route path={'/'} element={<Home/>}/>
+                                <Route path={'/notfound'} element={<NotFound/>}/>
+                                <Route path={'/*'} element={<NotFound/>}/>
+                            </Routes>
+                        </main>
 
-                    <Footer/>
-                </div>
-            </BrowserRouter>
+                        <Footer/>
+                    </div>
+                </BrowserRouter>
+            </AlertProvider>
         </GithubProvider>
     );
 }
